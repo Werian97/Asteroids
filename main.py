28dd3eaa784a43f731fb547ecda3f7b49ce189ab
Emanuele <you@example.com>
2026-06-12 18:22:28 +0200

    fix updating scoring board number-bug

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
-from points import calculate_points, get_score
+from points import calculate_points, update_scoring_board
 import sys
 
 
@@ -70,26 +70,7 @@ def main():
 
     pygame.quit()
     print("Game over!")
-    points = calculate_points(destroyed)
-    print(f"You totalized {points} points")
-    name = input("Enter your name: ")
-
-    with open("classifica.txt", "r") as file:
-        classifica = file.read()
-    classifica_lines = classifica.split("\n")
-    placement = 0
-    for line in classifica_lines:
-        if points <= get_score(line):
-            placement += 1
-        else:
-            break
-    point_line = f"{name}: {points}"
-    new_classifica_lines = classifica_lines[0:placement]
-    new_classifica_lines.append(point_line)
-    new_classifica_lines.extend(classifica_lines[placement:len(classifica_lines)])
-    new_classifica = "\n".join(new_classifica_lines)
-    with open("classifica.txt", "w") as file:
-        file.write(new_classifica)
+    update_scoring_board(destroyed)
 
 
 if __name__ == "__main__":
--- points.py
+++ points.py
@@ -5,8 +5,27 @@ def calculate_points(destroyed) -> int:
     return points
 
 def get_score(line) -> int:
-    score = ""
-    for character in line:
-        if character in "0123456789":
-            score += character
-    return int(score) if score != "" else int("0")
+    line_pieces = line.split(" ")
+    return int(line_pieces[len(line_pieces)-1])
+
+def update_scoring_board(destroyed):
+    points = calculate_points(destroyed)
+    print(f"You totalized {points} points")
+    name = input("Enter your name: ")
+
+    with open("classifica.txt", "r") as file:
+        classifica = file.read()
+    classifica_lines = classifica.split("\n")
+    placement = 0
+    for line in classifica_lines:
+        if points <= get_score(line):
+            placement += 1
+        else:
+            break
+    point_line = f"{name}: {points}"
+    new_classifica_lines = classifica_lines[0:placement]
+    new_classifica_lines.append(point_line)
+    new_classifica_lines.extend(classifica_lines[placement:len(classifica_lines)])
+    new_classifica = "\n".join(new_classifica_lines)
+    with open("classifica.txt", "w") as file:
+        file.write(new_classifica)
