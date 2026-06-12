c3698c33f72b2d22dd1e86a125e52dfb308e7e72
Emanuele <you@example.com>
2026-06-12 18:36:20 +0200

    add exception when Classifica.txt has not been created yet

--- points.py
+++ points.py
@@ -13,19 +13,23 @@ def update_scoring_board(destroyed):
     print(f"You totalized {points} points")
     name = input("Enter your name: ")
 
-    with open("classifica.txt", "r") as file:
-        classifica = file.read()
-    classifica_lines = classifica.split("\n")
-    placement = 0
-    for line in classifica_lines:
-        if points <= get_score(line):
-            placement += 1
-        else:
-            break
-    point_line = f"{name}: {points}"
-    new_classifica_lines = classifica_lines[0:placement]
-    new_classifica_lines.append(point_line)
-    new_classifica_lines.extend(classifica_lines[placement:len(classifica_lines)])
-    new_classifica = "\n".join(new_classifica_lines)
-    with open("classifica.txt", "w") as file:
-        file.write(new_classifica)
+    try:
+        with open("Classifica.txt", "r") as file:
+            classifica = file.read()
+        classifica_lines = classifica.split("\n")
+        placement = 0
+        for line in classifica_lines:
+            if points <= get_score(line):
+                placement += 1
+            else:
+                break
+        point_line = f"{name}: {points}"
+        new_classifica_lines = classifica_lines[0:placement]
+        new_classifica_lines.append(point_line)
+        new_classifica_lines.extend(classifica_lines[placement:len(classifica_lines)])
+        new_classifica = "\n".join(new_classifica_lines)
+        with open("Classifica.txt", "w") as file:
+            file.write(new_classifica)
+    except:
+        file = open("Classifica.txt", "w")
+        file.write(f"{name}: {points}")
