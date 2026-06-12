6f113406a62156c5d7cd576648c4b4f559489904
Emanuele <you@example.com>
2026-06-12 11:31:01 +0200

    fix record-deletion bug

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def main():
     point_line = f"{name}: {points}"
     new_classifica_lines = classifica_lines[0:placement]
     new_classifica_lines.append(point_line)
-    new_classifica_lines.extend(classifica_lines[placement:len(classifica_lines)-1])
+    new_classifica_lines.extend(classifica_lines[placement:len(classifica_lines)])
     new_classifica = "\n".join(new_classifica_lines)
     with open("classifica.txt", "w") as file:
         file.write(new_classifica)
